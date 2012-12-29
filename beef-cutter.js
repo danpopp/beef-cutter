43ffc82291e8b1a3ceab840a42a17ab4e73db57d
danpopp <dan@danpopp.net>
2012-12-29 13:37:34 -0800

    Node.js Access for BeEF RESTful API
    Creates JSON object with authentication credentials, sends them via POST, makes a GET request with the token response from the POST to retrieve the hooks.

---
+++ beef-cutter.js
@@ -0,0 +1,52 @@
+//BeEF node.js RESTful API authentication and request:
+//By: Dan Popp (dan@comeandget.me) */* December 2012
+
+var beef = require('http');
+beefObject = JSON.stringify({
+        "username" : "beef",
+        "password" : "beef"
+});
+var postheaders = {
+        'Content-Type' : 'application/json',
+        'Content-Length' : Buffer.byteLength(beefObject, 'utf8')
+};
+var optionspost = {
+        host : 'localhost',
+        port : 3000,
+        path : '/api/admin/login',
+        method : 'POST',
+        headers : postheaders
+};
+var reqPost = beef.request(optionspost, function(res) {
+        res.on('data', function(beefchunk) {
+                console.info('\nBeEF Authorization Token:');
+                var beefobj = JSON.parse(beefchunk);
+                var token = beefobj.token;
+                process.stdout.write(token);
+                console.info('\n');
+                var hooks = '/api/hooks?token=' + token;
+                var optionsgetmsg = {
+                        host : 'localhost', 
+                        port : 3000,
+                        path : hooks,
+                        method : 'GET'
+                };
+                var reqGet = beef.request(optionsgetmsg, function(res) {
+                        res.on('data', function(beefchunk) {
+                                console.info('BODY: ' + beefchunk);
+                                var beefjson = JSON.parse(beefchunk);
+                                console.info(beefjson['hooked-browsers']);
+                        });
+
+                });
+                reqGet.end();
+                reqGet.on('error', function(e) {
+                        console.error(e);
+                });
+        });
+});
+reqPost.write(beefObject);
+reqPost.end();
+reqPost.on('error', function(e) {
+        console.error(e);
+});
