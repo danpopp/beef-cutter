992598efbf32c78f721b2f21a113905206f6ed69
danpopp <dan@danpopp.net>
2012-12-30 02:14:09 -0500

    Added -o and -x options using 'commander' package.

--- beef-cutter.js
+++ beef-cutter.js
@@ -1,15 +1,28 @@
-//Access to BeEF RESTful API for Node.js 
-//By: Dan Popp (dan@comeandget.me) */* December 2012
+#!/usr/bin/env node
+/**
+  * Access to BeEF RESTful API for Node.js 
+  * By: Dan Popp (dan@comeandget.me) - December 2012
+  */
+var program = require('commander');
+
+program
+  .version('BeEF-Cutter: BeEF RESTful API Access for Node.js\nVersion 0.0.3-alpha (codename: "shnickerdoodle")')
+  .option('-o, --online', 'List online hosts.')
+  .option('-x, --offline', 'List offline hosts.')
+  .parse(process.argv);
 
 var beef = require('http');
+
 beefObject = JSON.stringify({
         "username" : "beef",
         "password" : "beef"
 });
+
 var postheaders = {
         'Content-Type' : 'application/json',
         'Content-Length' : Buffer.byteLength(beefObject, 'utf8')
 };
+
 var optionspost = {
         host : 'localhost',
         port : 3000,
@@ -17,6 +30,7 @@ var optionspost = {
         method : 'POST',
         headers : postheaders
 };
+
 var reqPost = beef.request(optionspost, function(res) {
         res.on('data', function(beefchunk) {
                 console.info('\nBeEF Authorization Token:');
@@ -26,16 +40,22 @@ var reqPost = beef.request(optionspost, function(res) {
                 console.info('\n');
                 var hooks = '/api/hooks?token=' + token;
                 var optionsgetmsg = {
-                        host : 'localhost', 
+                        host : 'localhost',
                         port : 3000,
                         path : hooks,
                         method : 'GET'
                 };
+
+                if (program.online){
                 var reqGet = beef.request(optionsgetmsg, function(res) {
                         res.on('data', function(beefchunk) {
-                                console.info('BODY: ' + beefchunk + '\n');
+                                console.info('ONLINE HOSTS:\n');
                                 var beefjson = JSON.parse(beefchunk);
-                                console.info(beefjson['hooked-browsers']);
+                                var hookedlist = (beefjson['hooked-browsers'].online);
+                                for (i in hookedlist)
+                                {
+                                console.log(hookedlist[i].page_uri + '\n');
+                                }
                         });
 
                 });
@@ -43,6 +63,26 @@ var reqPost = beef.request(optionspost, function(res) {
                 reqGet.on('error', function(e) {
                         console.error(e);
                 });
+                }
+
+                if (program.offline){
+                var reqGet = beef.request(optionsgetmsg, function(res) {
+                        res.on('data', function(beefchunk) {
+                                console.info('OFFLINE HOSTS:\n');
+                                var beefjson = JSON.parse(beefchunk);
+                                var hookedlist = (beefjson['hooked-browsers'].offline);
+                                for (i in hookedlist)
+                                {
+                                console.log(hookedlist[i].page_uri + '\n');
+                                }
+                        });
+
+                });
+                reqGet.end();
+                reqGet.on('error', function(e) {
+                        console.error(e);
+                });
+                }
         });
 });
 reqPost.write(beefObject);
